1723f413350c5326d97a692e53272e8ff57df812
Saurabhjava <45967591+Saurabhjava@users.noreply.github.com>
2022-06-10 12:33:04 +0530

    Second Commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
 	public static void main(String as[]) {
-		System.out.println("Hello World. Testing this on Jenkins!");
+		System.out.println("Welcome To CTS Jenkins Training");
 	}
 }
