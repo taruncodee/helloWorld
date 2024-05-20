b4314025c1ceca1c4b35316d65a2a9b0020ce779
Saurabhjava <45967591+Saurabhjava@users.noreply.github.com>
2024-05-20 11:17:07 +0530

    Update HelloWorld.java with new line

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
 	public static void main(String as[]) {
 		System.out.println("Welcome To Brillio Jenkins Training");
+		System.out.println("Hello Java Team!");
+		
 	}
 }
